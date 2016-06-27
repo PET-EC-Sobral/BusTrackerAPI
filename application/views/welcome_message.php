1f1ee253e5458a57f30c433ab36a5f9f531d52c7
Laercio Santana <laerciosantana021@hotmail.com>
2016-06-27 01:15:33 -0300

    adds wellcome web page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Bus Tracker API</title>
 
 	<style type="text/css">
 
@@ -61,6 +61,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	#container {
 		margin: 10px;
 		border: 1px solid #D0D0D0;
+		height: 100%;
 		box-shadow: 0 0 8px #D0D0D0;
 	}
 	</style>
@@ -68,21 +69,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Bem vindo a Bus Tracker API</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	<div id="body">	 	
+		<p><a href="docs/index.html">Documentação</a></p>
+		<p><a href="https://github.com/LaercioSantana/BusTrackerAPI">Github</a></p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">Um projeto desenvolvido pelo grupo <a href="http://www.pet.ec.ufc.br/">PET - Engenharia da Computação UFC</a> </p>
 </div>
 
 </body>
