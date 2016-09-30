b2c325a4f5131312a47d78c7c0ec321642edc054
Laercio Santana <laerciosantana021@hotmail.com>
2016-09-30 09:49:46 -0300

    enable cross-origin resource sharing

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Token");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+	die();
+}
 /**
  * CodeIgniter
  *
